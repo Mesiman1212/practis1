d4568991930c6dcfb1443250a5301128b37265ac
Mesiman1212 <meseretberehan23@gmail.com>
2024-08-18 23:41:35 -1200

    update

--- anmite.js
+++ anmite.js
@@ -1,4 +1,4 @@
-          gsap.from("#box1" , {
+         var first= gsap.from("#box1" , {
     
     rotate:360,
     duration:10,
@@ -18,7 +18,7 @@ borderRadius:"100%"
 });
 
 gsap.to("#box3" , {
-    x:-1000,
+    x:100,
     y:1000,
     rotate:360,
     duration:10,
@@ -29,8 +29,41 @@ borderRadius:"100%"
 
 
 gsap.to("#box4" , {
-    x:1000,
-    y:-500,
+    x:2000,
+    y:100,
+    rotate:360,
+    duration:10,
+    delay:2,
+repeat:8,
+borderRadius:"100%"
+});
+gsap.from("#box5" , {
+    y:-20000,
+    x:0,
+    rotate:360,
+    duration:10,
+    delay:2,
+repeat:8,
+borderRadius:"100%"
+});gsap.from("#box6" , {
+    y:-25000,
+    x:100,
+    rotate:360,
+    duration:10,
+    delay:2,
+repeat:8,
+borderRadius:"100%"
+});gsap.from("#box7" , {
+    y:-20000,
+    x:200,
+    rotate:360,
+    duration:10,
+    delay:2,
+repeat:8,
+borderRadius:"100%"
+});gsap.from("#box8" , {
+    y:-1000,
+    x:600,
     rotate:360,
     duration:10,
     delay:2,
